b2c6cff572b70788fda7229524877baba4464360
Tenshi <158788527+basedTenshi@users.noreply.github.com>
2026-04-03 21:03:16 +0800

    Add agent boilerplate messages

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,15 @@ import os
 
 
 client = Groq(
-    api_key=os.environ.get("GF_KEY")
+    api_key=os.environ.get("GF_KEY"),
+    base_url="https://api.groq.com/openai/v1",
 )
 
+user_input = ""
 
+response = client.responses.create(
+    input=user_input,
+    model="openai/gpt-oss-20b"
+)
+
+response_output = response.output_text
